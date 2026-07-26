f65676664781fe48d00b4e4a51eed4b3049cd653
AdarshaLS <101311673+AdarshaLS@users.noreply.github.com>
2026-07-26 17:28:58 +0530

    Create quality_checks_silver.sql

---
+++ tests/quality_checks_silver.sql
@@ -0,0 +1,92 @@
+--24. Load Script01
+--Check For Nulls or duplicates in Primary Key
+--Expectation: No Result
+
+SELECT
+cst_id,
+COUNT(*)
+FROM bronze.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*)>1 OR cst_id is NULL;
+
+
+--Data filtering removing invalid
+SELECT *
+FROM(
+SELECT 
+*,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
+FROM bronze.crm_cust_info)
+t WHERE flag_last=1;
+
+
+
+--Check for unwanted Spaces
+--Expactation: No Results
+SELECT cst_firstname
+FROM bronze.crm_cust_info
+WHERE cst_firstname!=TRIM(cst_firstname);
+
+SELECT cst_lastname
+FROM bronze.crm_cust_info
+WHERE cst_lastname!=TRIM(cst_lastname);
+
+SELECT cst_gndr
+FROM bronze.crm_cust_info
+WHERE cst_gndr!=TRIM(cst_gndr);
+
+
+--Check For Nulls or duplicates in Primary Key
+--Expectation: No Result
+
+SELECT
+cst_id,
+COUNT(*)
+FROM bronze.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*)>1 OR cst_id is NULL;
+
+
+--Data filtering removing invalid
+SELECT *
+FROM(
+SELECT 
+*,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
+FROM bronze.crm_cust_info)
+t WHERE flag_last=1;
+
+
+-- Quality of silver 
+
+--Check for unwanted Spaces
+--Expactation: No Results
+SELECT cst_firstname
+FROM silver.crm_cust_info
+WHERE cst_firstname!=TRIM(cst_firstname);
+
+SELECT cst_lastname
+FROM silver.crm_cust_info
+WHERE cst_lastname!=TRIM(cst_lastname);
+
+SELECT cst_gndr
+FROM silver.crm_cust_info
+WHERE cst_gndr!=TRIM(cst_gndr);
+
+--Data Standardization & Consistency
+SELECT DISTINCT cst_gndr
+FROM silver.crm_cust_info;
+
+
+--Check Data Consistency: Between Sales, Quantity and price 
+-->> Sales = Quantity*Price
+-->> Values must not be NULL, Zero or negative
+SELECT DISTINCT
+sls_sales,
+sls_quantity,
+sls_price
+FROM 
+silver.crm_sales_details
+WHERE sls_sales ! = sls_quantity*sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <=0 OR sls_quantity <=0 OR sls_price <=0;
